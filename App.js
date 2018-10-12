ee8d89cfe9797f9b667a2456456386aec93825c0
Italo Lino <italolino@gmail.com>
2018-10-12 00:12:59 -0300

    Style and state refactor

--- App.js
+++ App.js
@@ -1,88 +1,74 @@
-import React , { Component } from 'react';
-import { StyleSheet, Text, View , TouchableOpacity  } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 import { Directions } from 'react-native-gesture-handler';
-  
-
 
 
 export default class App extends React.Component {
-  constructor(props){
+
+  constructor(props) {
     super(props)
-    this.state = {golTeamOne: 0 , golTeamTwo: 0, resultadoOne: 0, resultadoTwo: 0 }
+
+    this.state = {
+      golTeamOne: 0,
+      golTeamTwo: 0,
+      resultadoOne: 0,
+      resultadoTwo: 0
+    }
   }
-    golTeamOne = () => {
+  golTeamOne = () => {
 
-      this.state.golTeamOne = this.state.golTeamOne +1
-      let goal = this.state.golTeamOne
+    let newGoal = this.state.golTeamOne + 1
+    this.setState({ golTeamOne: newGoal })
 
-      let stateOne = this.state
+  }
+  golTeamTwo = () => {
 
-      stateOne.resultadoOne = goal
+    let newGoal = this.state.golTeamTwo + 1
+    this.setState({ golTeamTwo: newGoal })
 
-      this.setState(stateOne)
-      
-    }
-    golTeamTwo = () => {
-      this.state.golTeamTwo = this.state.golTeamTwo + 1
-      let goal = this.state.golTeamTwo
+  }
 
-      let stateTwo = this.state
+  desmarkGolOne = () => {
 
-      stateTwo.resultadoTwo = goal
-      this.setState(stateTwo)
+    let newGoal = this.state.golTeamOne - 1
+    this.setState({ golTeamOne: newGoal })
+  }
+  desmarkGolTwo = () => {
 
-
-    }
-
-    desmarkGolOne = () => {
-      
-      this.state.golTeamOne = this.state.golTeamOne - 1
-      let goal = this.state.golTeamOne
-
-      let stateOne = this.state
-
-      stateOne.resultadoOne = goal
-
-      this.setState(stateOne)
-
-    }
-    desmarkGolTwo = () => {
-
-      this.state.golTeamTwo = this.state.golTeamTwo - 1
-      let goal = this.state.golTeamTwo
-
-      let stateTwo = this.state
-
-      stateTwo.resultadoTwo = goal
-      this.setState(stateTwo)
-
-
-    }
+    let newGoal = this.state.golTeamTwo - 1
+    this.setState({ golTeamTwo: newGoal })
+  }
 
 
   render() {
     return (
       <View style={styles.container}>
-        
-        <View style={styles.teamStruct}>
-        
-          <Text>TeamOne</Text>
-          <Text>TeamTwo</Text>
-
+        <View style={styles.teamOneContainer}>
+          <Text style={styles.numberLabel}>{this.state.golTeamOne}</Text>
+          <Text style={styles.teamNameLabel}>Time Rodrigo</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.buttonRemove} onPress={() => this.desmarkGolOne()}>
+              <Text style={styles.buttonLabel}>-</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonAdd} onPress={() => this.golTeamOne()}>
+              <Text style={styles.buttonLabel}>+</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.teamStructTwo}>
-          <Text style={styles.result}>{this.state.resultadoOne}</Text>
-
-          <Text style={styles.result}>{this.state.resultadoTwo}</Text>
+        <View style={styles.teamTwoContainer}>
+          <Text style={styles.numberLabel}>{this.state.golTeamTwo}</Text>
+          <Text style={styles.teamNameLabel}>Time Italo</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.buttonRemove} onPress={() => this.desmarkGolTwo()}>
+              <Text style={styles.buttonLabel}>-</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonAdd} onPress={() => this.golTeamTwo()}>
+              <Text style={styles.buttonLabel}>+</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.buttonStruct}>
-          <TouchableOpacity style={styles.button} onPress= {this.golTeamOne} ><Text style={styles.buttonText}>GolTeamOne</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress= {this.desmarkGolOne} ><Text style={styles.buttonText}>TiraTeamOne</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress= {this.golTeamTwo} ><Text style={styles.buttonText}>GolTeamTwo</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress= {this.desmarkGolTwo} ><Text style={styles.buttonText}>TiraTeamTwo</Text></TouchableOpacity>
-        </View>
-      </View>
+      </View >
     );
   }
 }
@@ -91,38 +77,58 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7d7d',
-    paddingTop: 100,
+    justifyContent: 'center',
+    alignItems: 'stretch'
   },
-  teamStruct:{
-    flexDirection:'row',
-    justifyContent: 'space-around', 
-    backgroundColor:'#4f4f',
-
-  },
-  teamStructTwo:{
+  teamOneContainer: {
     flex: 1,
-    justifyContent: 'space-around',
-    flexDirection:'row',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    margin: 35,
+    padding: 10,
   },
-  result:{
-    width: 150,
-    height: 100,
-    color:'lightgray',
-    fontSize:65,
-    backgroundColor: "#fff",
-  },
-  buttonStruct:{
+  teamTwoContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    margin: 25,
+    padding: 10,
   },
-  button:{
-    width:100,
-    height:100,
+  numberLabel: {
+    flex: 3,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 70,
+    fontWeight: 'bold',
+    color: 'black',
   },
-  buttonText:{
-    fontSize: 15,
-    backgroundColor: '#fff',
+  teamNameLabel: {
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 25,
+  },
+  buttonContainer: {
+    flex: 2,
+    flexDirection: 'row'
+  },
+  buttonAdd: {
+    flex: 1,
+    backgroundColor: 'green',
+    borderRadius: 15,
+    margin: 15
+  },
+  buttonRemove: {
+    flex: 1,
+    backgroundColor: 'red',
+    borderRadius: 15,
+    margin: 15
+  },
+  buttonLabel: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 30,
+    color: 'white'
   }
 });
 
